221973f203b4fdddc889d65afa8ccdd1914e8178
agruezo <agruezo@gmail.com>
2022-05-01 14:06:32 -0400

    updated issue with date formatting in JS

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -66,7 +66,7 @@ const ExpenseForm = (props) => {
         const expenseData = {
             title: enteredTitle,
             amount: enteredAmount,
-            date: new Date(enteredDate)
+            date: new Date(enteredDate.replace(/-/g, '\/')),
         };
 
         console.log(expenseData);
